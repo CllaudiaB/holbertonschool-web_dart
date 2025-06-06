fc4b9901600f7980767d530edd33fbfbeb470158
CllaudiaB <buraanna27@gmail.com>
2025-06-06 13:43:24 +0200

    task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,9 +1,8 @@
 void outer(String name, String id) {
 
     String inner() {
-        var lastName = name.split(" ");
-        var firstName = name.split(" ")[0];
-        return "Hello Agent ${lastName[1][0]}.$firstName , your id is $id";
+        var parts = name.split(" ");
+        return "Hello Agent ${parts[1][0]}.${parts[0]}, your id is $id";
     }
 
     print(inner());
